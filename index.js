31f4402ef8ceb4484fdfe5e67c5a03f4a844df9b
AnitaMuna <ojadianita@gmail.com>
2020-03-26 23:02:54 +0100

    added first js task

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let myName = 'Anita Ojadi'
+
+let courses = ['html', 'css', 'javascript', 'c#', 'python', 'coding'];
+
+console.log('My name is ${myName}, my courses are: ${courses}');
+
+
+for (x=0; x<=200; x++)
+{
+    if (x % 2 === 0){
+        console.log(x);
+    }
+};
